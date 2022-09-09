f3f47c39d95902dea43f7499bca0618a7f045691
maria.jumova@intexsoft.by <marfavasilevna@inbox.ru>
2022-09-09 16:29:38 +0300

    Add function buildThree

--- src/App.js
+++ src/App.js
@@ -57,11 +57,16 @@ const Folder = ({ name, children }) => {
         <AiOutlineFolder />
         <span>{name}</span>
       </div>
-      <Collapsible isOpen={isOpen}>{children?.map(x => {
-              return x.type === "file" ? <Tree.File key={x.name} name={x.name}/> : <Tree.Folder key={x.name} name={x.name} children={x.childrens}/>
+      <Collapsible isOpen={isOpen}>
+        {children?.map(x => {
+          return x.type === "file" ? (
+            <Tree.File key={x.name} name={x.name} />
+          ) : (
+            <Tree.Folder key={x.name} name={x.name} children={x.childrens} />
+          )
             })}
       </Collapsible>
-      </StyledFolder>
+    </StyledFolder>
   );
 };
 
@@ -72,14 +77,54 @@ const Tree = ({ children }) => {
 Tree.File = File;
 Tree.Folder = Folder;
 
+const buildThree = (mapArrayParse) => {
+  const root = [];
+
+  mapArrayParse.forEach((element) => {
+    const obj = {
+      type: element.name.startsWith("file") ? "file" : "folder",
+      childrens: [],
+    };
+
+    if (element.name.includes("in")) {
+      let nestedElement = element.name.split("in")
+      
+      obj.name= nestedElement[0].trim();
+      obj.parent = nestedElement[1].trim();
+
+      let existence = root.find(
+        (r) => r.name === obj.name && r.parent === undefined,
+      )
+      if (existence) { 
+        root[root.indexOf(existence)] = obj
+      }
+      else root.push(obj) 
+    } else {
+      obj.name = element.name.trim();
+      root.push(obj)
+    }
+  });
+
+  root.slice(1).forEach((el) => {
+    if (el.parent !== undefined) {
+      let parent = root.find((r) => r.name === el.parent)
+      root[root.indexOf(parent)].childrens.push(el)
+    }
+  })
+  return {name: "root", type:"folder", childrens: root.slice(1).filter(x => x.parent === undefined)}
+};
+
+
 export const App = () => {
-  const rootFolder = data;
+  const rootFolder = buildThree(data);
   return (
     <StyledWrap>
-    <Tree>
-      <Tree.Folder name={rootFolder.name} children={rootFolder.childrens}>
-      </Tree.Folder>
-    </Tree>
-  </StyledWrap>
+      <Tree>
+        <Tree.Folder 
+          name={rootFolder.name} 
+          children={rootFolder.childrens}>
+        </Tree.Folder>
+      </Tree>
+    </StyledWrap>
   );
 }
